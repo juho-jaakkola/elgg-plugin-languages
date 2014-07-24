bbea56e5b155aa7e68e83a990c421aa0e6f14faf
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2014-07-24 19:11:06 +0300

    Updated translations for latest Elgg 1.9 version.

--- comment_tracker/languages/fi.php
+++ comment_tracker/languages/fi.php
@@ -1,27 +1,26 @@
 <?php
 /**
- * comment_tracker language extender
- * 
- * @package ElggCommentTracker
- * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
- * @copyright Copyright (c) 2007-2011 Cubet Technologies. (http://cubettechnologies.com)
- * @version  1.0
- * @author Akhilesh @ Cubet Technologies
+ * Finnish translations for the comment_tracker plugin
  */
 
-$finnish = array(
+return array(
 	'comments' => "Kommentit",
 	'comment:notification:settings' => 'Kommentti-ilmoitukset',
 	'comment:notification:settings:description' => 'Valitse tavat, joilla haluat saada ilmoituksen, kun joku kommentoi seuraamiasi kohteita.',
 	'comment:notification:settings:how' => 'Valitse ilmoitustavat',
 	'comment:notification:settings:linktext' => 'Näytä kaikki kohteet, joista olet tilannut ilmoitukset',
-	'comment:notification:settings:success' => 'Ilmoitusasetuksesi tallennettu.',
+	'comment:subscriptions' => 'Subscriptions',
+	'comment:subscriptions:none' => 'Ei tilauksia',
+	'comment:subscribe:tooltip' => 'Tilaa ilmoitukset siitä kun joku lisää kommentin',
 	'allow:comment:notification' => 'Haluatko ottaa käyttöön ilmoitukset? ',
 	'email:content:type' => 'Haluatko ottaa käyttään HTML-sähköpostit? ',
 	'text:email' => 'Ei',
 	'html:email' => 'Kyllä',
 	'comment:subscribe' => 'Seuraa',
 	'comment:unsubscribe' => 'Älä seuraa',
+	'comment:subscribe:long' => 'Tilaa ilmoitukset uusista kommenteista',
+	'comment:unsubscribe:long' => 'Peru ilmoitukset uusista kommenteista',
+	'comment_tracker:setting:autosubscribe' => "Tilaa automaattisesti ilmoitukset kaikista sisällöistä, joita itse kommentoit?",
 	'show:subscribers' => 'Näytä seuraajat',
 	'comment:subscribe:success' => 'Seuraat nyt tätä kohdetta ja saat jatkossa ilmoitukset uusista kommenteista.',
 	'comment:subscribe:failed' => "Kohteen ilmoitusten tilaaminen epäonnistui!",
@@ -30,121 +29,32 @@ $finnish = array(
 	'comment:unsubscribe:failed' => "Tilauksen peruminen epäonnistui!",
 	'comment:unsubscribe:not:valid:url' => 'Syötit virheellisen osoitteen tilauksen perumiseen.',
 	'comment:unsubscribe:entity:not:access' => "Kohdetta ei löytynyt.",
-	'comment:notify:subject' => 'Kohteessa "%s" on uusi kommentti',
-	'comment:notify:group:subject' => 'Kohteessa "%s" on uusi kommentti',
-	'comment:notify:body:web' => 'Hei %s,
-<br/>Kohteessa "%s" on uusi kommentti
-<br/>%s lisäsi kommentin:
-<br />%s
-<br/>%s
-<br/>
-<font color="#888888" size="2">
-Sait tämän viestin, koska olet tilannut ilmoitukset viestissä mainitusta kohteesta.<br/>
-Voit muuttaa viestiasetuksiasi täällä:
-<br/>%s
-<br/>Voit peruuttaa tilauksen täällä:
-<br/>%s
-</font>
-		',
-  	'comment:notify:group:body:web' => 'Hei %s,
-<br/>Kohteessa "%s" on uusi viesti
-<br/>%s lisäsi viestin:
-<br />%s
-<br/>%s
-<br/>
-<font color="#888888" size="2">
-Sait tämän viestin, koska olet tilannut ilmoitukset viestissä mainitusta kohteesta.<br/>
-Voit muuttaa viestiasetuksiasi täällä:
-<br/>%s
-<br/>Voit peruuttaa tilauksen täällä:
-<br/>%s
-</font>
-		',
-	'comment:notify:body:email:text' => 'Hei %s,
+	'comment_tracker:setting:show_button' => "Näytä Tilaa/Peru -painikkeet kommenttikentän yhteydessä? ",
+	'comment_tracker:item' => "kohde",
+	'comment_tracker:setting:notify_owner' => "Lähetä myös käyttäjän omat ilmoitukset comment_trackerin kautta?",
 
-Kohteessa "%s" on uusi kommentti
+	'comment:notify:subject:groupforumtopic' => '%s vastasi keskusteluun %s ryhmässä %s',
+	'comment:notify:subject:comment' => '%s kommentoi %s "%s"',
+	'comment:notify:subject:comment:group' => '%s kommentoi %s "%s" ryhmässä %s',
 
-%s lisäsi kommentin:
-
-%s
-
-%s
-
-Terveisin
-%s
-
-_________________________________________________________________________________
-<font color="#888888" size="2">
-Huomioi, että kohteen näkeminen saattaa vaatia kirjautumisen.
-
-Sait tämän viestin, koska olet tilannut ilmoitukset viestissä mainitusta kohteesta.
-Voit muuttaa viestiasetuksiasi täällä:
-%s
-Voit peruuttaa tilauksen täällä:
-%s
-</font>
-		',
-  	'comment:notify:group:body:email:text' => 'Hei %s,
-
-Kohteessa "%s" on uusi viesti
-
-%s lisäsi viestin:
-
-%s
-
-%s
-
-Terveisin,
-%s
-
-_________________________________________________________________________________
-<font color="#888888" size="2">
-Huomioi, että kohteen näkeminen saattaa vaatia kirjautumisen.
-
-Sait tämän viestin, koska olet tilannut ilmoitukset viestissä mainitusta kohteesta.
-Voit muuttaa viestiasetuksiasi täällä:
-%s
-Voit peruuttaa tilauksen täällä:
-%s
-</font>
-		',
-
-'comment:notify:body:email:html' => '
-<div>
-	<div>Hi %s,</div>
-	<div>There is a new comment on %s</div>
-	<div>%s wrote:</div>
-	<div>%s</div>
-	<div>%s</div>
-	<div>&nbsp;</div>
-	<div>Regards,</div></div>
-	<div>%s</div>
-	<div>&nbsp;</div>
-	<div style="border-top:1px solid #CCCCCC;color:#888888;">
-		<div>Please note you may have to login before viewing the post or topic.</div>
-		<div>You have received this notification because you have subscribed to it, or are involved in it.</div>
-		<div>To change your notification preferences for all such messages, please click here: %s</div>
-		<div>To unsubscribe from this particular post or topic, please click here: %s</div>
-	</div>
-</div>
-		',
-  	'comment:notify:group:body:email:html' => '
-<div>
-	<div>Hi %s,</div>
-	<div>There is a new post in the thread %s</div>
-	<div>%s wrote: %s</div>
-	<div>%s</div>
-	<div>&nbsp;</div>
-	<div>Regards,</div>
-	<div>%s</div>
-	<div style="border-top:1px solid #CCCCCC;color:#888888;">
-		<div>Please note you may have to login before viewing the post or topic.</div>
-		<div>You have received this notification because you have subscribed to it, or are involved in it.</div>
-		<div>To change your notification preferences for all such messages, please click here: %s</div>
-		<div>Or,to unsubscribe from this particular post or topic, please click here: %s</div>
-	</div>
-</div>
-		',
+	'comment:notify:subject:blog' => '%s kommentoi blogia "%s"',
+	'comment:notify:subject:blog:group' => '%s kommentoi blogia "%s" ryhmässä %s',
+	'comment:notify:subject:bookmarks' => '%s kommentoi kirjanmerkkiä "%s"',
+	'comment:notify:subject:bookmarks:group' => '%s kommentoi kirjanmerkkiä "%s" ryhmässä %s',
+	'comment:notify:subject:file' => '%s kommentoi tiedostoa "%s"',
+	'comment:notify:subject:file:group' => '%s kommentoi tiedostoa "%s" ryhmässä %s',
+	'comment:notify:subject:event' => '%s kommentoi tapahtumaa "%s"',
+	'comment:notify:subject:event:group' => '%s kommentoi tapahtumaa "%s" ryhmässä %s',
+	'comment:notify:subject:page' => '%s kommentoi sivua "%s"',
+	'comment:notify:subject:page:group' => '%s kommentoi sivua "%s" ryhmässä %s',
+	'comment:notify:subject:page_top' => '%s kommentoi sivua "%s"',
+	'comment:notify:subject:page_top:group' => '%s kommentoi sivua "%s" ryhmässä %s',
+	'comment:notify:subject:poll' => '%s kommentoi kyselyä "%s"',
+	'comment:notify:subject:poll:group' => '%s kommentoi kyselyä "%s" ryhmässä %s',
+	'comment:notify:subject:album' => '%s kommentoi albumia "%s"',
+	'comment:notify:subject:album:group' => '%s kommentoi albumia "%s" ryhmässä %s',
+	'comment:notify:subject:image' => '%s kommentoi kuvaa "%s"',
+	'comment:notify:subject:image:group' => '%s kommentoi kuvaa "%s" ryhmässä %s',
+	'comment:notify:subject:sharemaps' => '%s kommentoi karttaa "%s"',
+	'comment:notify:subject:sharemaps:group' => '%s kommentoi karttaa "%s ryhmässä %s"',
 );
-				
-add_translation("fi", $finnish);
