3d2750f80a80d8be0e067d75e51b91eac1aac403
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2014-07-24 18:27:33 +0300

    Added translations for html_email_handler plugin.

---
+++ html_email_handler/languages/fi.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+	'html_email_handler' => "HTML E-mail Handler",
+
+	'html_email_handler:theme_preview:menu' => "HTML-ilmoitus",
+
+	// settings
+	'html_email_handler:settings:notifications:description' => "Ottaessasi tämän asetuksen käyttöön, kaikki sivuston sähköpostit lähetetään HTML-muodossa.",
+	'html_email_handler:settings:notifications' => "Aseta sivuston oletukseksi",
+	'html_email_handler:settings:notifications:subtext' => "Ota käyttöön lähettääksesi kaikki sivuston sähköpostit oletuksena HTML-muodossa",
+
+	'html_email_handler:settings:sendmail_options' => "Valinnaiset sendmail-parametrit",
+	'html_email_handler:settings:sendmail_options:description' => "Voit määrittää valinnaisia sendmail-parametreja, kuten -f %s (ehkäistäksesi sähköpostien merkitsemistä roskapostiksi)",
+
+	// notification body
+	'html_email_handler:notification:footer:settings' => "Voit määrittää ilmoitusasetuksesi %stästä%s",
+);
